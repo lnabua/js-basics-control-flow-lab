a719a9f54176bd655fd3dcb2162496319d3a2ada
lnabua <nabua.lorraine@gmail.com>
2021-02-23 23:48:13 -0800

    commit tree 71580f00a3ff676d85dec7cdf59c394dff7c42fb

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num1){
+  let result
+  if (num1 <= 400) {
+    result = 'This one is on me!';
+  }
+  else if (num1 > 2000 && num1 < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (num1 > 2500) {
+    result = 'No can do.';
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return (cityName === "NYC" ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(expr){
+  switch (expr) {
+    case expr = 'generous':
+      return 'Thank you so much.';
+      break;
+    case expr = 'not as generous':
+      return 'Thank you.';
+      break;
+    case expr = 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
 }
